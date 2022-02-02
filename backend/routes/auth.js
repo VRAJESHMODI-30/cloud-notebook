429648d253c43c268070d6be17e93ec8cb1b1d5f
Vrajesh Modi <91773413+VRAJESHMODI-30@users.noreply.github.com>
2022-02-02 20:22:47 +0530

    Create a endpoint for fetching userData

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchuser");
 
 const JWT_SECRET = "TOPSECRET";
 
-//Create a User using: POST "/api/auth/createuser". Doesn't require Auth
+//ROUTE 1: Create a User using: POST "/api/auth/createuser". Doesn't require Auth
 router.post(
   "/createuser",
   [
@@ -52,7 +53,7 @@ router.post(
     }
   }
 );
-//Authenticate a User using: POST "/api/auth/login".
+//ROUTE 2: Authenticate a User using: POST "/api/auth/login". No-login required
 router.post(
   "/login",
   [
@@ -89,5 +90,17 @@ router.post(
     }
   }
 );
+
+//ROUTE 3: Get loggedIn User details using: POST "/api/auth/getuser". Login required
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server ERROR!");
+  }
+});
 
 module.exports = router;
